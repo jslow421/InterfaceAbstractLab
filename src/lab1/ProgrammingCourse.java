009d3525757860c4728f80068bae7e45d1879233
jslowik <jslowik@10.30.20.75>
2015-09-14 19:04:32 -0500

    moved methods to abstract class from intro

--- src/lab1/ProgrammingCourse.java
+++ src/lab1/ProgrammingCourse.java
@@ -54,11 +54,17 @@ public abstract class ProgrammingCourse {
         }
         this.credits = credits;
     }
-    
         public String getPrerequisites() {
         return prerequisites;
     }
+
+    public void setPrerequisites(String prerequisites) {
+        if(prerequisites == null || prerequisites.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: prerequisites cannot be null of empty string");
+            System.exit(0);
+        }
+        this.prerequisites = prerequisites;
         
-    public abstract void setPrerequisites();
-        
+    }
 }
