2c9ef8cea0287adf0ce09342b3a51713619a6a99
jslowik <jslowik@10.30.20.92>
2015-09-16 14:50:14 -0500

    attempted to answer questions

--- src/lab2/CourseInfo.java
+++ src/lab2/CourseInfo.java
@@ -34,5 +34,22 @@ public class CourseInfo {
         System.out.println("Course Number: " + Java2.CourseNumber());
         System.out.println("Course Credits: " + Java2.CourseCredits());
         System.out.println("Prerequisites: " + Java2.Prerequisites());
+        
+        /*
+        I think this is a better overall way of handling this program vs. using
+        an abstract class as the super class. There is no inherited code which
+        proved to be ideal for the prerequisites as each course had to implement
+        them differently. This was only slightly more "work" than using the 
+        abstract class as the other methods could be inherited with no issue
+        since they were concrete. 
+        
+        I think Liskov's Substitution Principle applies to the Java intro course
+        and the advanced java course because they have the exact same behaviors
+        only different data.
+        
+        I don't believe it would apply to intro to programming because this class
+        behaves slightly differntly without any prerequisites.
+        
+        */
     }
 }
